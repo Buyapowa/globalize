5091c4ae24f7d82a67237c4d7fa7dbf4ada5070b
Michael Baldry <michael@brightbits.co.uk>
2015-04-03 15:08:31 +0100

    Making use uuid for the reference column

--- lib/globalize/active_record/migration.rb
+++ lib/globalize/active_record/migration.rb
@@ -67,7 +67,7 @@ module Globalize
         end
 
         def create_translation_table
-          connection.create_table(translations_table_name, id: primary_key_type) do |t|
+          connection.create_table(translations_table_name) do |t|
             t.references table_name.sub(/^#{table_name_prefix}/, '').singularize, null: false, type: primary_key_type
             t.string :locale, :null => false
             t.timestamps :null => false
